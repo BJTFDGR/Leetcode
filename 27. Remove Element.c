860b773978e17b76ee64db5bd9f629deadf41afd
Mark Chen <chenbcsjtu@gmail.com>
2021-03-04 10:17:22 +0800

    Create 27. Remove Element.c
    Given an array nums and a value val, remove all instances of that value in-place and return the new length.
    
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    
    The order of elements can be changed. It doesn't matter what you leave beyond the new length.
    
    Clarification:
    
    Confused why the returned value is an integer but your answer is an array?
    
    Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
    
    Internally you can think of this:
    
    // nums is passed in by reference. (i.e., without making a copy)
    int len = removeElement(nums, val);
    
    // any modification to nums in your function would be known by the caller.
    // using the length returned by your function, it prints the first len elements.
    for (int i = 0; i < len; i++) {
        print(nums[i]);
    }
     
    
    Example 1:
    
    Input: nums = [3,2,2,3], val = 3
    Output: 2, nums = [2,2]
    Explanation: Your function should return length = 2, with the first two elements of nums being 2.
    It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.

---
+++ 27. Remove Element.c
@@ -0,0 +1,32 @@
+
+
+// 双指针
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		int length = nums.size(), index = 0;
+		while ( index < length )
+		{
+			 if ( nums[index] == val )
+			 {
+				 nums[index] = nums[length - 1];
+				 -- length;
+			 }
+				
+			else ++index;
+		}
+		 return length;  
+	}
+};
+
+//快慢指针
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
